cd25ad75dc97f857f81937596552c20dd32becef
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-15 12:27:32 -0500

    created data json file for text info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import './index.css';
 
 function App() {
+  
+
   return (
     <div className="main-container">
       <h1>Are homeless people really drugs addicts and alcoholics?</h1>
