a14ff900c2e90daeeecc2552edf46a74ebafafdc
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-15 13:05:27 -0500

    created resources section

--- src/App.js
+++ src/App.js
@@ -8,17 +8,42 @@ function App() {
 
   return (
     <div className="main">
+
       <div className="main-container">
         <h1>Are homeless people really drugs addicts and alcoholics?</h1>
       </div>
+
       <div className="main-container-content">
+
         <div className="text-content1">
           <p>
             {data["1"]}
           </p>
         </div>
+
         <img className="image image1-container" src={image1} alt=""/>
+
+        <div className="text-content1">
+          <p>
+            {data["2"]}
+          </p>
+        </div>
+
       </div>
+
+      <div className="resources-container">
+        <h1>Resources</h1>
+        {
+          data["resources"].map((resource, index) => {
+            return (
+              <div className="spacing" key={index}>
+                {resource}
+              </div>
+            );
+          })
+        }
+      </div>
+
     </div>
   );
 }
