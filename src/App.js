3fdd17020b007c0c6def9d873d91790a59666434
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-15 12:53:33 -0500

    styled containers with image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import './index.css';
+import data from './data.json';
+import image1 from './images/homeless1.jpg';
 
 function App() {
-  
+
 
   return (
-    <div className="main-container">
-      <h1>Are homeless people really drugs addicts and alcoholics?</h1>
+    <div className="main">
+      <div className="main-container">
+        <h1>Are homeless people really drugs addicts and alcoholics?</h1>
+      </div>
+      <div className="main-container-content">
+        <div className="text-content1">
+          <p>
+            {data["1"]}
+          </p>
+        </div>
+        <img className="image image1-container" src={image1} alt=""/>
+      </div>
     </div>
   );
 }
